5f8e11d132f33c965d170c77ff4ebfc6aa4569df
ahnaf daiyan <77898156+barta3235@users.noreply.github.com>
2024-02-27 00:11:31 +0600

    Add files via upload

--- nine_map_scoping_recap.js
+++ nine_map_scoping_recap.js
@@ -22,4 +22,37 @@
   // here the console.log(y) will give error because the 
   // var y is hoisted,yes, but to the top of the function
   // outside the functional block the var is not accessible
-   
+  
+  
+
+const func1= ()=>{
+     let count=0;
+
+
+     const func2= ()=>{
+        count++;
+        console.log(count);
+     }
+
+     return func2;
+}
+
+const x1= func1();
+console.log(x1);
+x1();
+x1();
+
+
+
+
+const person={
+  name: "Ahnaf",
+  id:1234,
+}
+
+const identity = ({name,id}) => {
+   console.log(name);
+   console.log(id);
+}
+
+identity(person);
